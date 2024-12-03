7cecb4e49e0d4db73f06c4e5cefaecd0ae9dc647
sura <surakaraali@gmail.com>
2024-12-03 17:02:16 +0300

    Index sayfasında bazı değişiklikler

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,7 @@
 namespace YourProject.Models
 {
+    using System.Collections.Generic;
+
     public class Movie
     {
         public string Title { get; set; }
@@ -7,35 +9,27 @@ namespace YourProject.Models
         public string PosterUrl { get; set; }
         public double Rating { get; set; }
         public int ReleaseYear { get; set; }
-        public string Genres { get; set; }
-        public string Genres1 { get; set; }
-        public string Genres2 { get; set; }
-        public string Genres3 { get; set; } 
+        public List<string> Genres { get; set; } // Genres artık bir liste
         public string MovieHour { get; set; }
-        public string Director{ get; set; }
+        public string Director { get; set; }
         public string Writer { get; set; }
-        public string Actors { get; set; }
-        public string Actors1 { get; set; }
-        public string Actors2 { get; set; }
+        public List<string> Actors { get; set; } // Actors da bir liste oldu
 
-
-        public Movie(string title, string description, string posterUrl, double rating, int releaseYear, string genres, string genres1, string genres2, string genres3, string movieHour,string director,string writer, string actors, string actors1,string actors2)
+        public Movie(string title, string description, string posterUrl, double rating, int releaseYear, List<string> genres, string movieHour, string director, string writer, List<string> actors)
         {
             Title = title;
             Description = description;
             PosterUrl = posterUrl;
             Rating = rating;
             ReleaseYear = releaseYear;
-            Genres = genres; 
-            Genres1=genres1;
-            Genres2=genres2;
-            Genres3=genres3;
-            MovieHour=movieHour;
-            Director =director;
-            Writer=writer;
-            Actors=actors;
-            Actors1=actors1;
-            Actors2=actors2;
+            Genres = genres;
+            MovieHour = movieHour;
+            Director = director;
+            Writer = writer;
+            Actors = actors;
         }
     }
 }
+
+
+    
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,33 +1,164 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using YourProject.Models; 
+using YourProject.Models;
 using System.Collections.Generic;
 
 namespace YourProject.Pages
 {
-    public class IndexModel : PageModel  
+    public class IndexModel : PageModel
     {
         public List<Movie>? Movies { get; set; }
 
-        public void OnGet() 
+        public void OnGet()
         {
-            
+
             Movies = new List<Movie>
-            {
-            new Movie("The Godfather","Michael, the young and idealistic son of Vito Corleone, the head of the most powerful Mafia clan in New York, returns home as a war hero and is determined to live his own life. But tragic circumstances make him face the legacy of his family.","/img/godfather.jpeg",9.2,1972,"Gangster","Tragedy","Drama","Crime","2h 55m","Francis Ford Coppola","Mario Puzo and Francis Ford Coppola","Marlon Brando","Al Pacino","James Caan"),
-            new Movie("Fight Club","An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.","/img/fight_club.jpeg",8.8,1999,"Drama","Psychologic","Action","Adventure","2h 19m","David Fincher","Chuck Palahniuk and Jim Uhls","Brad Pitt","Edward Norton","Meat Loaf"),
-            new Movie("Cumhuriyet Şarkısı", "A thief who steals corporate secrets through dream-sharing technology.", "/img/cumhuriyet_sarkısı.jpg", 8.8, 2010,"Drama","History","Documentery","Romantic","2h 8m","Yağız Alp Akaydın","BKM","Salih Bademci","Birce Akalay","Ertan Saban"),
-            new Movie("Matrix","When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.","/img/matrix.jpeg",8.7,1999,"Action","Sci-fi","Epic","Adventure","2h 16m","Lana Wachowski and Lilly Wachowski","Lana Wachowski and Lilly Wachowski","Keanu Reeves","Laurence Fishburne"," Carrie-Anne Moss"),
-            new Movie("Panda Bear Africa", "A panda travels from China to Africa, facing hippos, hyenas, gorillas. He rescues his kidnapped dragon friend, makes new friends, and protects their jungle home.", "/img/panda_bear_africa.jpeg",5.5, 2024,"Animation","Family","Comedy","Adventure","1h 24 m","Karsten Killerich and Richard Claus","Rob Sprackling","Maurits Delchot","Thom Hoffman","Silas Lekgoathi"),
-            new Movie("Inside Out 2", "A sequel that features Riley entering puberty and experiencing brand new, more complex emotions as a result. As Riley tries to adapt to her teenage years, her old emotions try to adapt to the possibility of being replaced.", "/img/inside_out_2.jpeg", 7.6, 2024,"Animation","Adventure","Comedy","Family","1h 36m","Kelsey Menn","Meg LeFauve and Dave Holstein","Amy Poehler","Maya Hawke","Kensington Tallman"),
-            new Movie("Inside Out", "After young Riley is uprooted from her Midwest life and moved to San Francisco, her emotions - Joy, Fear, Anger, Disgust and Sadness - conflict on how best to navigate a new city, house, and school.", "/img/inside_out.jpeg", 8.1, 2015,"Animation","Adventure","Comedy","Family","1h 35m","Pete Docter and Ronnie Del Carmen","Pete Docter, Ronnie Del Carmen and Meg LeFauve","Amy Poehler","Bill Hader","Lewis Black"),
-            new Movie("Interstellar","When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.","/img/interstellar.jpeg",8.7,2014,"Sci-fi","Epic","Adventure","Drama","2h 49m","Christopher Nolan","Jonathan Nolan and Christopher Nolan","Matthew McConaughey","Anne Hathaway","Jessica Chastain"),
-            new Movie("The Prestige","Set in London in the 1800s, two rival magicians read each others diary containing the secrets of their magic tricks and personal life. As we go back in time when the diaries were written, both magicians become obsessed with their rival's best trick. The tricks, as shown to the audience, look the same, but neither magician can figure out how his opponent does it.","/img/the_prestige.jpeg",8.5,2006,"Drama","Mystery","Sci-fi","Thriller","2h 10m","Christopher Nolan","Jonathan NolanChristopher and NolanChristopher Priest","Christian Bale","Hugh Jackman","Scarlett Johansson"),
-            new Movie("The Silence of the Lambs","A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.","/img/the_silence_of_the_lambs.jpeg",8.6,1991,"Psychological","Thriller","Drama","Crime","1h 58m","Jonathan Demme","Thomas Harris and Ted Tally","Jodie Foster","Anthony Hopkins","Scott Glenn"),
-            new Movie("The Star Wars","After the Empire overpowers the Rebel Alliance, Luke Skywalker begins his Jedi training with Yoda. At the same time, Darth Vader and bounty hunter Boba Fett pursue his friends across the galaxy.","/img/the_star_wars.jpeg",8.7,1980,"Sci-fi","Epic","Action","Fantasy","2h 4m", "Irvin Kershner","Leigh Brackett and Lawrence KasdanGeorge Lucas","Mark Hamill","arrison Ford","Carrie Fisher"),
-            new Movie("The Schindler's List","In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.","/img/schindler_s_list.jpeg",9,1993,"Historical","Epic","Drama","Biography","3h 15m","Steven Spielberg","Thomas Keneally and teven Zaillian","Liam Neeson","Ralph Fiennes","Ben Kingsley")
-                
-            };
+{
+    new Movie(
+        title: "The Godfather",
+        description: "Michael, the young and idealistic son of Vito Corleone, the head of the most powerful Mafia clan in New York, returns home as a war hero and is determined to live his own life. But tragic circumstances make him face the legacy of his family.",
+        posterUrl: "/img/godfather.jpeg",
+        rating: 9.2,
+        releaseYear: 1972,
+        genres: new List<string> {"Gangster","Tragedy","Drama","Crime"},
+        movieHour: "2h 55m",
+        director: "Francis Ford Coppola",
+        writer: "Mario Puzo and Francis Ford Coppola",
+        actors: new List<string> {"Marlon Brando","Al Pacino","James Caan"}
+    ),
+    new Movie(
+        title: "Fight Club",
+        description: "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
+        posterUrl: "/img/fight_club.jpeg",
+        rating: 8.8,
+        releaseYear: 1999,
+        genres: new List<string> {"Drama", "Psychologic", "Action", "Adventure"},
+        movieHour: "2h 19m",
+        director: "David Fincher",
+        writer: "Chuck Palahniuk and Jim Uhls",
+        actors: new List<string> { "Brad Pitt", "Edward Norton", "Meat Loaf" }
+    ),
+    new Movie(
+        title: "Cumhuriyet Şarkısı",
+        description: "A thief who steals corporate secrets through dream-sharing technology.",
+        posterUrl: "/img/cumhuriyet_sarkısı.jpg",
+        rating: 8.8,
+        releaseYear: 2010,
+        genres: new List<string> { "Drama", "History", "Documentary", "Romantic" },
+        movieHour: "2h 8m",
+        director: "Yağız Alp Akaydın",
+        writer: "BKM",
+        actors: new List<string> { "Salih Bademci", "Birce Akalay", "Ertan Saban" }
+    ),
+    new Movie(
+        title: "Matrix",
+        description: "When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.",
+        posterUrl: "/img/matrix.jpeg",
+        rating: 8.7,
+        releaseYear: 1999,
+        genres: new List<string> { "Action", "Sci-fi", "Epic", "Adventure" },
+        movieHour: "2h 16m",
+        director: "Lana Wachowski and Lilly Wachowski",
+        writer: "Lana Wachowski and Lilly Wachowski",
+        actors: new List<string> { "Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss" }
+    ),
+    new Movie(
+        title: "Panda Bear Africa",
+        description: "A panda travels from China to Africa, facing hippos, hyenas, gorillas. He rescues his kidnapped dragon friend, makes new friends, and protects their jungle home.",
+        posterUrl: "/img/panda_bear_africa.jpeg",
+        rating: 5.5,
+        releaseYear: 2024,
+        genres: new List<string> { "Animation", "Family", "Comedy", "Adventure" },
+        movieHour: "1h 24m",
+        director: "Karsten Killerich and Richard Claus",
+        writer: "Rob Sprackling",
+        actors: new List<string> { "Maurits Delchot", "Thom Hoffman", "Silas Lekgoathi" }
+    ),
+    new Movie(
+        title: "Inside Out 2",
+        description: "A sequel that features Riley entering puberty and experiencing brand new, more complex emotions as a result. As Riley tries to adapt to her teenage years, her old emotions try to adapt to the possibility of being replaced.",
+        posterUrl: "/img/inside_out_2.jpeg",
+        rating: 7.6,
+        releaseYear: 2024,
+        genres: new List<string> { "Animation", "Adventure", "Comedy", "Family" },
+        movieHour: "1h 36m",
+        director: "Kelsey Menn",
+        writer: "Meg LeFauve and Dave Holstein",
+        actors: new List<string> { "Amy Poehler", "Maya Hawke", "Kensington Tallman" }
+    ),
+    new Movie(
+        title: "Inside Out",
+        description: "After young Riley is uprooted from her Midwest life and moved to San Francisco, her emotions - Joy, Fear, Anger, Disgust and Sadness - conflict on how best to navigate a new city, house, and school.",
+        posterUrl: "/img/inside_out.jpeg",
+        rating: 8.1,
+        releaseYear: 2015,
+        genres: new List<string> { "Animation", "Adventure", "Comedy", "Family" },
+        movieHour: "1h 35m",
+        director: "Pete Docter and Ronnie Del Carmen",
+        writer: "Pete Docter, Ronnie Del Carmen and Meg LeFauve",
+        actors: new List<string> { "Amy Poehler", "Bill Hader", "Lewis Black" }
+    ),
+    new Movie(
+        title: "Interstellar",
+        description: "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
+        posterUrl: "/img/interstellar.jpeg",
+        rating: 8.7,
+        releaseYear: 2014,
+        genres: new List<string> { "Sci-fi", "Epic", "Adventure", "Drama" },
+        movieHour: "2h 49m",
+        director: "Christopher Nolan",
+        writer: "Jonathan Nolan and Christopher Nolan",
+        actors: new List<string> { "Matthew McConaughey", "Anne Hathaway", "Jessica Chastain" }
+    ),
+    new Movie(
+        title: "The Prestige",
+        description: "Set in London in the 1800s, two rival magicians read each others diary containing the secrets of their magic tricks and personal life. As we go back in time when the diaries were written, both magicians become obsessed with their rival's best trick.",
+        posterUrl: "/img/the_prestige.jpeg",
+        rating: 8.5,
+        releaseYear: 2006,
+        genres: new List<string> { "Drama", "Mystery", "Sci-fi", "Thriller" },
+        movieHour: "2h 10m",
+        director: "Christopher Nolan",
+        writer: "Jonathan Nolan and Christopher Priest",
+        actors: new List<string> { "Christian Bale", "Hugh Jackman", "Scarlett Johansson" }
+    ),
+    new Movie(
+        title: "The Silence of the Lambs",
+        description: "A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.",
+        posterUrl: "/img/the_silence_of_the_lambs.jpeg",
+        rating: 8.6,
+        releaseYear: 1991,
+        genres: new List<string> { "Psychological", "Thriller", "Drama", "Crime" },
+        movieHour: "1h 58m",
+        director: "Jonathan Demme",
+        writer: "Thomas Harris and Ted Tally",
+        actors: new List<string> { "Jodie Foster", "Anthony Hopkins", "Scott Glenn" }
+    ),
+    new Movie(
+        title: "The Star Wars",
+        description: "After the Empire overpowers the Rebel Alliance, Luke Skywalker begins his Jedi training with Yoda. At the same time, Darth Vader and bounty hunter Boba Fett pursue his friends across the galaxy.",
+        posterUrl: "/img/the_star_wars.jpeg",
+        rating: 8.7,
+        releaseYear: 1980,
+        genres: new List<string> { "Sci-fi", "Epic", "Action", "Fantasy" },
+        movieHour: "2h 4m",
+        director: "Irvin Kershner",
+        writer: "Leigh Brackett and Lawrence Kasdan George Lucas",
+        actors: new List<string> { "Mark Hamill", "Harrison Ford", "Carrie Fisher" }
+    ),
+    new Movie(
+        title: "The Schindler's List",
+        description: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
+        posterUrl: "/img/schindler_s_list.jpeg",
+        rating: 9.0,
+        releaseYear: 1993,
+        genres: new List<string> { "Historical", "Epic", "Drama", "Biography" },
+        movieHour: "3h 15m",
+        director: "Steven Spielberg",
+        writer: "Thomas Keneally and Steven Zaillian",
+        actors: new List<string> { "Liam Neeson", "Ralph Fiennes", "Ben Kingsley" }
+    )
+};
+
         }
     }
 }
-
